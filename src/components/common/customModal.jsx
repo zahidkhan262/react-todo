ef584ddc603d7b15672c83f2047b6dabfc1029f1
zahidkhan262 <zahidkhan007860@gmail.com>
2022-01-31 00:13:45 +0530

    custome modale

--- src/components/common/customModal.jsx
+++ src/components/common/customModal.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './modal.css'
+import '../todo.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 
 const CustomModal = () => {
 
+    const [showModal, setShowModal] = useState(false)
+
+    const modalsHide = () => {
+        setShowModal(!showModal)
+        modalsHide()
+    }
 
     return (
         <>
-            <div className="modals-container">
-                <div className="modals-body">
-                    <h3>Id:1</h3>
-                    <h2>Edit Todo</h2>
-                    <button>close</button>
-                </div>
-            </div>
+            {
+                showModal && (
+                    <div className="modals_container">
+                        <div className="modals_body">
+                            <div className='modal_header d-flex-1'>
+                                <h3>Edit Todo</h3>
+                                <h3 title='cancel' onClick={modalsHide}><FontAwesomeIcon icon={faTimes} style={{ color: 'crimson' }} /></h3>
+                            </div>
+                            <div className="input_field">
+                                <input type="text" placeholder='Enter your task here.....'
+                                />
+                                <button type='submit' className='add_btn' ><FontAwesomeIcon icon={faEdit} /> Edit</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
         </>
     )
 };
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -3,6 +3,8 @@ import CustomModal from './common/CustomModal';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './todo.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
 const Todo = () => {
 
     const [inputValue, setInputValue] = useState("");
@@ -15,8 +17,9 @@ const Todo = () => {
         e.preventDefault();
         if (!inputValue) {
             toast.error("Invalid Input")
+        } else if (inputValue <= 3) {
+            toast.error("Input is lesser")
         } else {
-
             todo.push({ task_name: inputValue, id: new Date().getTime().toString() })
             setTodo(todo)
             toast.success("Task Added")
@@ -27,14 +30,22 @@ const Todo = () => {
     // delete todo button
     const deleteTodo = (index) => {
         const newTodo = todo.filter((ele, i) => {
-            return index !== i
+            return index !== ele.i
         })
         setTodo(newTodo)
 
         console.log(newTodo)
     }
+
+    // edit Todo by find method
+
+    const editTodo = (index) => {
+        console.log(index)
+    }
+
     return (
         <>
+
             <div className="container-1">
                 <div className="form_box">
                     <form onSubmit={addTodo}>
@@ -43,7 +54,7 @@ const Todo = () => {
                             <input type="text" placeholder='Enter your task here.....'
                                 value={inputValue} onChange={(e) => { setInputValue(e.target.value) }}
                             />
-                            <button type='submit' className='add_btn'>Add</button>
+                            <button type='submit' className='add_btn'><FontAwesomeIcon icon={faPlus} /> Add</button>
                         </div>
                     </form>
                     <ul className="todo_list">
@@ -55,20 +66,20 @@ const Todo = () => {
                                     <li className='d-flex-1' key={index}>
                                         <p>  {item.task_name} </p>
                                         <span>
-                                            <button className='edit_btn'>Edit</button>
+                                            <button className='edit_btn' onClick={() => editTodo(index)}>Edit</button>
                                             <button className='delete_btn' onClick={() => { deleteTodo(index) }}>Delete</button>
                                         </span>
                                     </li>
 
                                 )
                             })
-                                : <h3 className='center'>There is no Data..</h3>
+                                : <h3 className='center'>There is no <FontAwesomeIcon icon={faTimes} style={{ color: "crimson" }} /> Data..</h3>
                         }
                     </ul>
-
                 </div>
-                <CustomModal />
+                <CustomModal editTodo={editTodo} />
             </div>
+
             <ToastContainer
                 position="top-right"
                 autoClose={5000}
