4a0240716b97889d6f1b1eaebf99418880743add
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-06-13 12:13:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ import ReduxCounter from './redux/ReduxCounter';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-
+function useOutsideAlerter(ref) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        alert("You clicked outside of me!");
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
 
 function App() {
 
