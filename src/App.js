8544f4fbea91ecbe56b8e074adfa78cbb83ab1b6
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-03 22:12:28 +0530

    form++

--- src/App.js
+++ src/App.js
@@ -27,6 +27,42 @@ function useOutsideAlerter(ref) {
 function App() {
   return (
     <>
+//     form
+    import React, { useState } from 'react'
+
+const useForm = () => {
+    
+    //Form values
+    const [values, setValues] = useState({});
+    //Errors
+    const [errors, setErrors] = useState({});
+
+
+  //A method to handle form inputs
+    const handleChange = (event) => {
+        //To stop default events    
+        event.persist();
+
+        let name = event.target.name;
+        let val = event.target.value;
+
+        //Let's set these values in state
+        setValues({
+            ...values,
+            [name]:val,
+        })
+
+    }
+
+
+    return {
+        values,
+        errors,
+        handleChange,
+    }
+}
+
+export default useForm
       <RouterConfig />
       <Provider store={store}>
           <ReduxCounter />
