18b107d52fb9b232d085405317271e63d4b1d845
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-12 20:51:16 +0530

    break points

--- src/components/common/customModal.jsx
+++ src/components/common/customModal.jsx
@@ -4,7 +4,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 
-
+{
+  "responsive-preview": {
+    "Mobile": [
+      320,
+      675
+    ],
+    "Tablet": [
+      1024,
+      765
+    ],
+    "Desktop": [
+      1400,
+      800
+    ],
+    "Desktop  HD": [
+      1920,
+      1080
+    ]
+  }
+}
 
     
 const CustomModal = ({...props}) => {
