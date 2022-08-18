e2f7c62876945be5aa93aafafea781781212ed6f
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-18 22:01:06 +0530

    image upload ++

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -112,4 +112,61 @@ const Todo = () => {
     );
 }
 
+
+// image uploasder
+const uploadedImage = React.useRef(null);
+  const imageUploader = React.useRef(null);
+
+  const handleImageUpload = e => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      const { current } = uploadedImage;
+      current.file = file;
+      reader.onload = e => {
+        current.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        ref={imageUploader}
+        style={{
+          display: "none"
+        }}
+      />
+      <div
+        style={{
+          height: "60px",
+          width: "60px",
+          border: "1px dashed black"
+        }}
+        onClick={() => imageUploader.current.click()}
+      >
+        <img
+          ref={uploadedImage}
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute"
+          }}
+        />
+      </div>
+      Click to upload Image
+    </div>
+  );
+
 export default Todo;
