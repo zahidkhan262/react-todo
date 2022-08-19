84140b9baea1818acc34884339b29c80ca2e9d41
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-19 23:29:06 +0530

    update card

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -30,3 +30,87 @@ const PostCard = () => {
 }
 
 export default PostCard
+
+// after dynamic data
+
+import React, { useEffect, useState } from 'react'
+import './Post.css';
+import { faEllipsisVertical, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Col, Image, OverlayTrigger, Popover } from 'react-bootstrap';
+import { dltPost, favPost } from '../../redux/action/postAction';
+import { useDispatch, useSelector } from 'react-redux';
+import PROFILE_PIC from '../images/profile.jpg'
+import POST_PIC from '../images/post2.jfif'
+// import { useNavigate } from 'react-router-dom';
+const PostCard = ({ post, id }) => {
+  const [isLikePost, setIsLikePost] = useState(false);
+  const { fullname, designation, companyAddress, companyName } = post
+
+  // console.log(post, "postcard")
+
+  const favPosts = useSelector((state) => state.post.favouritePost);
+  const dispatch = useDispatch();
+  // const navigate = useNavigate()
+
+  useEffect(() => {
+    favPosts && favPosts.map((p, i) => {
+      if (i === id) {
+        setIsLikePost(true);
+      }
+      console.log("first")
+    })
+  }, [favPosts]);
+
+  // delete post
+  const deletePost = (id) => {
+    dispatch(dltPost(id))
+  }
+
+  // fav post
+  const addFav = (post) => {
+    dispatch(favPost(post))
+    // setIsLikePost(true)
+  }
+
+  const popover = (
+    <Popover id="popover-basic" className='pop-overs'>
+      <Popover.Header onClick={() => deletePost(id)}>Delete</Popover.Header>
+      <Popover.Header>Edit</Popover.Header>
+    </Popover>
+  );
+
+
+  return (
+    <>
+      <Col md={3}>
+        <div className="cards">
+          <div className="cards-header d-flex align-items-center justify-content-between">
+            <div className="cards-user-detail d-flex align-items-center justify-content-start">
+              <div className="cards-user-profile"><Image src={PROFILE_PIC} alt="profile" /></div>
+              <div className="cards-user-info text-white ">
+                <h6 className='mt-2 mb-0'>{fullname}</h6>
+                <p className='mb-0'>{designation}</p>
+              </div>
+            </div>
+            <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+              <FontAwesomeIcon icon={faEllipsisVertical} className="elipsis-icon" />
+            </OverlayTrigger>
+          </div>
+          <div className="cards-body">
+            <div className="cards-img"><Image src={POST_PIC} alt="post" /></div>
+          </div>
+          <div className='cards-footer d-flex justify-content-between align-items-center '>
+            <div className="cards-about">
+              <p>{companyName}</p>
+              <p>{companyAddress}</p>
+            </div>
+            <FontAwesomeIcon icon={faHeart} style={isLikePost ? { color: 'crimson' } : { color: 'gray' }} className='heart-icon' onClick={() => !isLikePost ? addFav(post) : undefined} />
+          </div>
+        </div>
+      </Col>
+    </>
+  )
+}
+
+export default PostCard
