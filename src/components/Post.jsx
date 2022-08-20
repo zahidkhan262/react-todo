8fd03b5db06486c2d46b835ae3f3b488bad08bb5
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-20 19:33:24 +0530

    UPDATE++

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -26,3 +26,44 @@ const Post = () => {
 }
 
 export default Post
+
+
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+import { CREATEPOST } from '../../router/constant'
+import PostCard from './PostCard'
+
+const Post = () => {
+    const { posts } = useSelector((state) => state.post)
+    const getImage = useSelector((state) => state.post.posts);
+    console.log(getImage, "//////////")
+    console.log(posts[2], "getpost")
+    const navigate = useNavigate()
+    return (
+        <>
+            <Container className='fixHeigth'>
+                <Row>
+                    <Col md={{ span: '2', offset: '10' }} className='mt-3'>
+                        <Button onClick={() => navigate(CREATEPOST)} className='fs-4'>Create Post<FontAwesomeIcon icon={faPlus} className='mx-1' /></Button>
+                    </Col>
+                    {
+                        posts && posts?.map((post, idx) => {
+                            console.log(post)
+                            return (
+                                <React.Fragment key={idx}>
+                                    <PostCard post={post} id={idx} />
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+export default Post
