53c8a321c82598402d5734cf0952c2a4f60a295e
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-22 23:39:17 +0530

    update++

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -6,16 +6,18 @@ import { Col, Image, OverlayTrigger, Popover } from 'react-bootstrap';
 import { dltPost, favPost } from '../../redux/action/postAction';
 import { useDispatch, useSelector } from 'react-redux';
 import PROFILE_PIC from '../images/profile.jpg'
-import POST_PIC from '../images/post2.jfif'
+import { useNavigate } from 'react-router';
+import { EDITPOST } from '../../router/constant';
 // import { useNavigate } from 'react-router-dom';
 const PostCard = ({ post, id }) => {
 
-  const { fullname, designation, companyAddress, companyName } = post
+  const { fullname, designation, companyAddress, companyName, imgUrl } = post
   const [isLikePost, setIsLikePost] = useState(false);
   const [isFav, setIsFav] = useState(false);
-  // console.log(post, "postcard")
+
+  // console.log(postIndex, "postIndex")
   const dispatch = useDispatch();
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
 
   // redux
   const favPosts = useSelector((state) => state.post.favouritePost);
@@ -37,11 +39,19 @@ const PostCard = ({ post, id }) => {
   const addFav = (post) => {
     dispatch(favPost(post))
   }
+  // Edit POST
+  const handleEditPost = (id) => {
+    navigate(`${EDITPOST}/${id}`)
+
+    // console.log(id, ',//////////')
+  }
 
   const popover = (
     <Popover id="popover-basic" className='pop-overs'>
       <Popover.Header onClick={() => deletePost(id)}>Delete</Popover.Header>
-      <Popover.Header>Edit</Popover.Header>
+      <Popover.Header onClick={() => {
+        handleEditPost(id)
+      }}>Edit</Popover.Header>
       <Popover.Header onClick={() => !isFav ? addFav(post) : undefined}>Favourite</Popover.Header>
     </Popover>
   );
@@ -49,7 +59,7 @@ const PostCard = ({ post, id }) => {
 
   return (
     <>
-      <Col md={3}>
+      <Col lg={4} md={4} xs={6}>
         <div className="cards">
           <div className="cards-header d-flex align-items-center justify-content-between">
             <div className="cards-user-detail d-flex align-items-center justify-content-start">
@@ -64,7 +74,7 @@ const PostCard = ({ post, id }) => {
             </OverlayTrigger>
           </div>
           <div className="cards-body">
-            <div className="cards-img"><Image src='' alt="post" /></div>
+            <div className="cards-img"><Image src={imgUrl} alt="post" /></div>
           </div>
           <div className='cards-footer d-flex justify-content-between align-items-center '>
             <div className="cards-about">
