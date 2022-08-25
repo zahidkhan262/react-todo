53a2cebf3ac8d9455fd4da22cb8a1234835de08b
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-25 23:40:17 +0530

    page not found

---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NotFound from './page.gif'
+const PageNotFound = () => {
+    return (
+        <>
+            <div className='page-not-found'><img src={NotFound} alt="no Data" /></div>
+        </>
+    )
+}
+
+export default PageNotFound
