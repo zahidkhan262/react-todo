d46f728532fc1bbd208e67ad751161e8503f822f
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-25 23:41:08 +0530

    protected routing++

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import TodoView from '../components/todo/TodoView'
 import Header from '../layout/Header'
 import Footer from '../layout/Footer'
-import { CREATEPOST, EDITPOST, FAVPOST, INDEX, LOGIN, LOGOUT, POST, REGISTER, TODO } from './constant'
+import { CREATEPOST, EDITPOST, FAVPOST, INDEX, LOGIN, LOGOUT, PAGENOTFOUND, POST, REGISTER, TODO } from './constant'
 import Home from '../layout/Home'
 import Post from '../components/post/Post'
 import CreatePost from '../components/post/CreatePost'
@@ -12,20 +12,25 @@ import EditPost from '../components/post/EditPost'
 import Register from '../components/auth/Register'
 import Login from '../components/auth/Login'
 import Logout from '../components/auth/Logout'
+import ProtectRouting from './ProtectRouting'
+import PageNotFound from '../layout/PageNotFound'
 const Routing = () => {
     return (
         <>
             <Header />
             <Routes>
-                <Route path={INDEX} element={<Home />} />
-                <Route path={TODO} element={<TodoView />} />
-                <Route path={CREATEPOST} element={<CreatePost />} />
-                <Route path={POST} element={<Post />} />
-                <Route path={`${EDITPOST}/:id`} element={<EditPost />} />
-                <Route path={FAVPOST} element={<FavPost />} />
                 <Route path={REGISTER} element={<Register />} />
-                <Route path={LOGOUT} element={<Logout />} />
                 <Route path={LOGIN} element={<Login />} />
+                <Route path={PAGENOTFOUND} element={<PageNotFound />} />
+                <Route element={<ProtectRouting />}>
+                    <Route path={INDEX} element={<Home />} exact />
+                    <Route path={TODO} element={<TodoView />} />
+                    <Route path={CREATEPOST} element={<CreatePost />} />
+                    <Route path={POST} element={<Post />} />
+                    <Route path={`${EDITPOST}/:id`} element={<EditPost />} />
+                    <Route path={FAVPOST} element={<FavPost />} />
+                    <Route path={LOGOUT} element={<Logout />} />
+                </Route>
             </Routes>
             <Footer />
         </>
