a12f5dc69fd0ad83a79eeac748e1774b8506abb8
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-25 23:34:30 +0530

    update header++

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { faSignOut } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Container } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom'
 import { logoutForm } from '../redux/action/authAction';
 import { FAVPOST, INDEX, LOGIN, POST, REGISTER, TODO } from '../router/constant';
 import './Layout.css'
 const Header = () => {
-    const user = useSelector((state) => state.auth.user)
-    console.log(user, "logggg")
+    const isAuthUser = useSelector((state) => state.auth.user)
     const favPosts = useSelector((state) => state.post.favouritePost);
+    const [signToggle, setSignToggle] = useState(false)
     const dispatch = useDispatch()
     return (
         <>
-            <header className='d-flex justify-content-between align-items-center'>
-                <div className="logo fs-4 text-secondary"><NavLink to={INDEX}>LOGO</NavLink></div>
-                <nav>
-                    <li>
-                        <NavLink to={INDEX}>Home</NavLink></li>
-                    <li><NavLink to={TODO}>CRUD</NavLink></li>
-                    <li> <NavLink to={POST}>Post</NavLink></li>
-                    {
-                        favPosts && favPosts.length
-                            ?
-                            <li> <NavLink to={FAVPOST}>Favourite</NavLink></li>
-                            : null
-                    }
-                    <div className='dropdown-profile'>Profile
-                        <div className="dropdown-content">
-                            <li><NavLink to={REGISTER}>Register</NavLink></li>
+            <header>
+                <Container>
+                    <div className='d-flex justify-content-between align-items-center'>
+                        <div className="logo fs-4 text-secondary"><NavLink to={INDEX}>LOGO</NavLink></div>
+                        <nav>
                             {
-                                user && user
+                                isAuthUser && isAuthUser
                                     ?
-                                    <li><NavLink to='' onClick={() => dispatch(logoutForm())}>Logout</NavLink></li>
+                                    <>
+                                        <li><NavLink to={INDEX}>Home</NavLink></li>
+                                        <li><NavLink to={TODO}>CRUD</NavLink></li>
+                                        <li> <NavLink to={POST}>Post</NavLink></li>
+                                        {
+                                            favPosts && favPosts.length
+                                                ?
+                                                <li> <NavLink to={FAVPOST}>Favourite</NavLink></li>
+                                                : null
+                                        }
+                                        <div className='dropdown-profile' onClick={() => setSignToggle((prev) => !prev)}>
+                                            <span className='profile-logo' title={isAuthUser.firstname + isAuthUser.lastname.charAt(0).toUpperCase()}>
+                                                {isAuthUser.firstname.charAt(0).toUpperCase()}
+                                                {isAuthUser.lastname.charAt(0).toUpperCase()}
+                                            </span>
+                                        </div>
+                                    </>
                                     :
-                                    <li><NavLink to={LOGIN}>Login</NavLink></li>
+                                    <div className='dropdown-profile'>{isAuthUser ? "Profile" : "Get Started"}
+                                        <div className="dropdown-content">
+                                            <li><NavLink to={REGISTER}>Register</NavLink></li>
+                                            <li><NavLink to={LOGIN}>Login</NavLink></li>
+                                        </div>
+                                    </div>
+
                             }
-                        </div>
+                            {isAuthUser
+                                &&
+                                <li style={signToggle ? { display: 'none' } : null}><FontAwesomeIcon icon={faSignOut} className='logout-icon' onClick={() => dispatch(logoutForm())} /></li>
+                            }
+                        </nav>
                     </div>
-                </nav>
+                </Container>
             </header>
         </>
     )
