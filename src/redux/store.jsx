0bd92ba488f5e54287049d8cec795aef7439a7db
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-26 20:50:14 +0530

    store update

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -4,3 +4,25 @@ import RootReducer from "./RootReducer";
 const store =createStore(RootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
+
+
+// main store with local storage
+
+import { configureStore } from '@reduxjs/toolkit';
+import { loadState, saveState } from './redux/localStorage';
+import rootReducer from './redux/rootReducer';
+
+const persistedState = loadState();
+const store = configureStore({
+    reducer: rootReducer,
+    persistedState: persistedState
+})
+store.subscribe(() => {
+    saveState({
+        post: store.getState().post,
+        auth: store.getState().auth.userAuth
+    })
+})
+
+
+export default store;
