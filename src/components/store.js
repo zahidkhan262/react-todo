c4bd845afc7f7ac00fdacde41c00639a8a19de5e
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-08-30 11:16:55 -0700

    rtk ++

--- src/components/store.js
+++ src/components/store.js
@@ -17,3 +17,31 @@ store.subscribe(() => {
 
 export default store;
 
+
+
+// now change for RTK 30-aug-2022
+
+import { configureStore } from '@reduxjs/toolkit';
+// import { loadState, saveState } from './redux/localStorage';
+import authSlice from './features/authSlice';
+import todoReducer from './redux/reducer/todoReducer'
+import postReducer from './redux/reducer/postReducer'
+// const persistedState = loadState();
+const store = configureStore({
+    reducer: {
+        auth:authSlice,
+        todo: todoReducer,
+        post: postReducer,
+    }
+})
+// persistedState: persistedState
+// store.subscribe(() => {
+//     saveState({
+//         post: store.getState().post,
+//         auth: store.getState().auth.userAuth
+//     })
+// })
+
+
+export default store;
+
