0790e68fb07a764b615398d7d4fbec40f3f2e605
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-11-26 09:05:42 +0530

    Update server.js
    changes in server.js

--- src/node-postgressql/server.js
+++ src/node-postgressql/server.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
-
+const passport = reuire('passport')
 
 const corOptions = {
     origin: 'http://localhost:8000'
@@ -13,10 +13,13 @@ const corOptions = {
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use(passport.initialize());
 
 // routers
 app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/posts', require('./routes/postRoutes'))
+app.use('/api/auth', require('./routes/authRoutes'))
+
 
 // server start here...
 
