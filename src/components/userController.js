0ad4901cbc370564cb42bee6c8386e0562a088e5
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-12-22 02:06:02 +0530

    Update userController.js

--- src/components/userController.js
+++ src/components/userController.js
@@ -83,7 +83,7 @@ const loginUser = asyncHandler(async(req, res)=>{
 })
 
 //-----------------------Generate JWT token---------------------------------//
-
+// here we can also pass all user data..
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
         expiresIn : '30d'
